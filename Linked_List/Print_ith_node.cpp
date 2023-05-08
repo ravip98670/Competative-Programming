4c4bde85d78d0ae9a75058f8f32f622c87369698
ravip98670 <ravispandey2002@gmail.com>
2023-05-08 21:23:38 +0530

    printing_the_ith_node

---
+++ Linked_List/Print_ith_node.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+
+class Node
+{
+public:
+	int data;
+	Node *next;
+	Node(int data)
+	{
+		this->data = data;
+		this->next = NULL;
+	}
+};
+
+using namespace std;
+void printIthNode(Node *head, int i)
+{
+    //Write your code here
+	Node *temp = head;
+	int len = 0;
+	while(temp != NULL){
+		if(len == i)
+			cout<<temp->data<<endl;
+		len++;
+		temp = temp->next;
+	}
+}
+
+Node *takeinput()
+{
+	int data;
+	cin >> data;
+	Node *head = NULL, *tail = NULL;
+	while (data != -1)
+	{
+		Node *newNode = new Node(data);
+		if (head == NULL)
+		{
+			head = newNode;
+			tail = newNode;
+		}
+		else
+		{
+			tail->next = newNode;
+			tail = newNode;
+		}
+		cin >> data;
+	}
+	return head;
+}
+
+int main()
+{
+	int t;
+	cin >> t;
+	while (t--)
+	{
+		Node *head = takeinput();
+		int pos;
+		cin >> pos;
+		printIthNode(head, pos);
+		cout << endl;
+	}
+	return 0;
+}
