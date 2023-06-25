4856b391e2e7de1b7a2a698c669de1d86c698175
ravip98670 <ravispandey2002@gmail.com>
2023-06-25 17:20:36 +0530

    kReverse_Linked_List_using_Recursion

---
+++ Linked_List/kReverse_Linked_List.cpp
@@ -0,0 +1,86 @@
+#include <iostream>
+
+class Node
+{
+public:
+	int data;
+	Node *next;
+	Node(int data)
+	{
+		this->data = data;
+		this->next = NULL;
+	}
+};
+
+using namespace std;
+Node *kReverse(Node *head, int k)
+{
+	//Write your code here
+	if(head == NULL || head->next == NULL || k == 0)
+		return head;
+	Node *prev = NULL;
+	Node *curr = head;
+	Node *next = NULL;
+	int count = 0;
+	while(curr != NULL && count < k ){
+		next = curr->next;
+		curr->next = prev;
+		prev = curr;
+		curr = next;
+		count++;
+	}
+	Node *temp = kReverse(next,k);
+	head->next = temp;
+	return prev;
+}
+
+Node *takeinput()
+{
+	int data;
+	cin >> data;
+	Node *head = NULL, *tail = NULL;
+	while (data != -1)
+	{
+		Node *newnode = new Node(data);
+		if (head == NULL)
+		{
+			head = newnode;
+			tail = newnode;
+		}
+		else
+		{
+			tail->next = newnode;
+			tail = newnode;
+		}
+		cin >> data;
+	}
+	return head;
+}
+
+void print(Node *head)
+{
+	Node *temp = head;
+	while (temp != NULL)
+	{
+		cout << temp->data << " ";
+		temp = temp->next;
+	}
+	cout << endl;
+}
+
+int main()
+{
+	int t;
+	cin >> t;
+
+	while (t--)
+	{
+		Node *head = takeinput();
+		int k;
+		cin >> k;
+		head = kReverse(head, k);
+		print(head);
+	}
+	
+	return 0;
+}
