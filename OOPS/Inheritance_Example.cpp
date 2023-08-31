33c93ae92a9efb22e7dde257f9edf36a451fe413
ravip98670 <ravispandey2002@gmail.com>
2023-08-31 16:38:22 +0530

    Inheritance_Example

---
+++ OOPS/Inheritance_Example.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+using namespace std;
+class Vehicle {
+	private :
+		int maxSpeed;
+
+	protected :
+		int numTyres;
+
+	public :
+		string color;
+
+};
+
+class Car : public Vehicle {
+	public :
+		int numGears;
+
+
+		void print() {
+			cout << "NumTyres : " << numTyres << endl;
+			cout << "Color : " << color << endl;
+			cout << "Num gears : " << numGears << endl;
+	//		cout << "Max Speed : " << maxSpeed << endl;
+		}
+
+
+};
+
+
+
+int main() {
+	Vehicle v;
+
+	v.color = "Blue";
+	// v.maxSpeed = 100;
+	// v.numTyres = 4;
+
+
+	Car c;
+	c.color = "Black";
+//	c.numTyres = 4;
+	c.numGears = 5;
+
+    c.print();
+}
+
