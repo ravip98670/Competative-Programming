99becb6c40a3dcc0b11dddbbfd22454e0ce80bca
ravip98670 <ravispandey2002@gmail.com>
2023-09-05 11:41:51 +0530

    Printing_Tree_Level_Wise

---
+++ Trees/Print_Level_Wise.cpp
@@ -0,0 +1,98 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+using namespace std;
+
+template <typename T>
+class TreeNode {
+   public:
+    T data;
+    vector<TreeNode<T>*> children;
+
+    TreeNode(T data) { this->data = data; }
+
+    ~TreeNode() {
+        for (int i = 0; i < children.size(); i++) {
+            delete children[i];
+        }
+    }
+};
+
+/************************************************************
+ 
+    Following is the structure for the TreeNode class
+
+    template <typename T>
+    class TreeNode {
+     public:
+        T data;
+        vector<TreeNode<T>*> children;
+    
+        TreeNode(T data) {
+            this->data = data;
+        }
+    
+        ~TreeNode() {
+            for (int i = 0; i < children.size(); i++) {
+                delete children[i];
+            }
+        }
+    };
+
+************************************************************/
+#include <queue>
+void printLevelWise(TreeNode<int>* root) {
+    // Write your code here
+    if(root == NULL)
+        return;
+
+    queue<TreeNode<int>*> pendingNodes;
+    pendingNodes.push(root);
+    //cout<<pendingNodes.front()<<":";
+    while(pendingNodes.size() != 0){
+        TreeNode<int> *front = pendingNodes.front();
+         cout<<front->data<<":";
+        pendingNodes.pop();
+
+        for(int i=0;i<front->children.size();i++){
+            cout<<front->children[i]->data;
+            pendingNodes.push(front->children[i]);
+            if(i != front->children.size()-1)
+                cout<<",";
+        }
+        cout<<endl;
+
+
+    }
+    
+}
+
+TreeNode<int>* takeInputLevelWise() {
+    int rootData;
+    cin >> rootData;
+    TreeNode<int>* root = new TreeNode<int>(rootData);
+
+    queue<TreeNode<int>*> pendingNodes;
+
+    pendingNodes.push(root);
+    while (pendingNodes.size() != 0) {
+        TreeNode<int>* front = pendingNodes.front();
+        pendingNodes.pop();
+        int numChild;
+        cin >> numChild;
+        for (int i = 0; i < numChild; i++) {
+            int childData;
+            cin >> childData;
+            TreeNode<int>* child = new TreeNode<int>(childData);
+            front->children.push_back(child);
+            pendingNodes.push(child);
+        }
+    }
+
+    return root;
+}
+
+int main() {
+    TreeNode<int>* root = takeInputLevelWise();
+    printLevelWise(root);
+}
