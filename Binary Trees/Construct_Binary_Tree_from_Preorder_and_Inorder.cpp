78cf5f67587c261b524ce158c216d675eb3e356c
ravip98670 <ravispandey2002@gmail.com>
2023-09-12 17:12:20 +0530

    Construct_Binary_Tree_Using_PreOrder_And_Inorder

---
+++ Binary Trees/Construct_Binary_Tree_from_Preorder_and_Inorder.cpp
@@ -0,0 +1,129 @@
+#include <iostream>
+#include <queue>
+
+template <typename T>
+class BinaryTreeNode {
+   public:
+    T data;
+    BinaryTreeNode<T> *left;
+    BinaryTreeNode<T> *right;
+
+    BinaryTreeNode(T data) {
+        this->data = data;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+using namespace std;
+/***********************************************************
+	Following is the Binary Tree Node class structure
+
+	template <typename T>
+	class BinaryTreeNode {
+    	public : 
+    	T data;
+    	BinaryTreeNode<T> *left;
+    	BinaryTreeNode<T> *right;
+
+    	BinaryTreeNode(T data) {
+        	this -> data = data;
+        	left = NULL;
+        	right = NULL;
+    	}
+	};
+
+***********************************************************/
+BinaryTreeNode<int>* buildTreehelper(int *preorder,int pStart,int pEnd,int *inorder,int iStart,int iEnd){
+	if(pStart > pEnd || iStart > iEnd)
+		return NULL;
+	
+	int rVal = preorder[pStart];
+	BinaryTreeNode<int>* root = new BinaryTreeNode<int>(rVal);
+
+	int k = 0;
+	for(int i=iStart;i<=iEnd;i++){
+		if(rVal == inorder[i]){
+			k = i;
+			break;
+		}
+	}
+
+	root->left = buildTreehelper(preorder,pStart+1,pStart + (k-iStart),inorder,iStart,k-1);
+	root->right = buildTreehelper(preorder,pStart +(k-iStart) +1,pEnd,inorder,k+1,iEnd);
+
+	return root;
+}
+
+BinaryTreeNode<int>* buildTree(int *preorder, int preLength, int *inorder, int inLength) {
+    // Write your code here
+	return buildTreehelper(preorder,0,preLength-1,inorder,0,inLength-1);
+}
+
+BinaryTreeNode<int> *takeInput() {
+    int rootData;
+    cin >> rootData;
+    if (rootData == -1) {
+        return NULL;
+    }
+    BinaryTreeNode<int> *root = new BinaryTreeNode<int>(rootData);
+    queue<BinaryTreeNode<int> *> q;
+    q.push(root);
+    while (!q.empty()) {
+        BinaryTreeNode<int> *currentNode = q.front();
+        q.pop();
+        int leftChild, rightChild;
+        
+        cin >> leftChild;
+        if (leftChild != -1) {
+            BinaryTreeNode<int> *leftNode = new BinaryTreeNode<int>(leftChild);
+            currentNode->left = leftNode;
+            q.push(leftNode);
+        }
+
+        cin >> rightChild;
+        if (rightChild != -1) {
+            BinaryTreeNode<int> *rightNode =
+                new BinaryTreeNode<int>(rightChild);
+            currentNode->right = rightNode;
+            q.push(rightNode);
+        }
+    }
+    return root;
+}
+
+void printLevelATNewLine(BinaryTreeNode<int> *root) {
+    queue<BinaryTreeNode<int> *> q;
+    q.push(root);
+    q.push(NULL);
+    while (!q.empty()) {
+        BinaryTreeNode<int> *first = q.front();
+        q.pop();
+        if (first == NULL) {
+            if (q.empty()) {
+                break;
+            }
+            cout << endl;
+            q.push(NULL);
+            continue;
+        }
+        cout << first->data << " ";
+        if (first->left != NULL) {
+            q.push(first->left);
+        }
+        if (first->right != NULL) {
+            q.push(first->right);
+        }
+    }
+}
+
+int main() {
+    int size;
+    cin >> size;
+    int *pre = new int[size];
+    int *in = new int[size];
+    for (int i = 0; i < size; i++) cin >> pre[i];
+    for (int i = 0; i < size; i++) cin >> in[i];
+    BinaryTreeNode<int> *root = buildTree(pre, size, in, size);
+    printLevelATNewLine(root);
+}
