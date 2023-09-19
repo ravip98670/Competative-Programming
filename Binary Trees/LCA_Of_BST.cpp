04fd29d33587ba8e8b313c8427f668b7ac8c14e6
ravip98670 <ravispandey2002@gmail.com>
2023-09-20 02:48:14 +0530

    Problem_practise_on_BST

---
+++ Binary Trees/LCA_Of_BST.cpp
@@ -0,0 +1,106 @@
+#include <iostream>
+#include <queue>
+
+template <typename T>
+class BinaryTreeNode {
+   public:
+    T data;
+    BinaryTreeNode<T>* left;
+    BinaryTreeNode<T>* right;
+
+    BinaryTreeNode(T data) {
+        this->data = data;
+        left = NULL;
+        right = NULL;
+    }
+    ~BinaryTreeNode() {
+        if (left) delete left;
+        if (right) delete right;
+    }
+};
+
+using namespace std;
+/**********************************************************
+
+	Following is the Binary Tree Node class structure
+
+	template <typename T>
+	class BinaryTreeNode {
+    	public: 
+    	T data;
+    	BinaryTreeNode<T> *left;
+    	BinaryTreeNode<T> *right;
+
+    	BinaryTreeNode(T data) {
+        	this->data = data;
+        	left = NULL;
+        	right = NULL;
+    	}
+	};
+
+***********************************************************/
+
+int getLCA(BinaryTreeNode<int>* root , int val1 , int val2){
+    // Write your code here
+	if(root == NULL)
+		return -1;
+	if(root->data == val1 || root->data == val2)
+		return root->data;
+	
+	if(root->data < val1 && root->data < val2)
+		return getLCA(root->right,val1,val2);
+	else if(root->data > val1 && root->data > val2)
+		 return getLCA(root->left,val1,val2);
+
+	int leftLCA = getLCA(root->left, val1, val2);
+	int rightLCA = getLCA(root->right,val1,  val2);
+
+	
+	if(leftLCA == -1)
+		return rightLCA;
+	else if(rightLCA == -1)
+		return leftLCA;
+	else return root->data;
+}
+
+BinaryTreeNode<int>* takeInput() {
+    int rootData;
+    
+    cin >> rootData;
+    if (rootData == -1) {
+        return NULL;
+    }
+    BinaryTreeNode<int>* root = new BinaryTreeNode<int>(rootData);
+    queue<BinaryTreeNode<int>*> q;
+    q.push(root);
+    while (!q.empty()) {
+        BinaryTreeNode<int>* currentNode = q.front();
+        q.pop();
+        int leftChild, rightChild;
+        
+        cin >> leftChild;
+        if (leftChild != -1) {
+            BinaryTreeNode<int>* leftNode = new BinaryTreeNode<int>(leftChild);
+            currentNode->left = leftNode;
+            q.push(leftNode);
+        }
+    
+        cin >> rightChild;
+        if (rightChild != -1) {
+            BinaryTreeNode<int>* rightNode =
+                new BinaryTreeNode<int>(rightChild);
+            currentNode->right = rightNode;
+            q.push(rightNode);
+        }
+    }
+
+    return root;
+}
+
+int main() {
+    BinaryTreeNode<int>* root = takeInput();
+    int a, b;
+    cin >> a >> b;
+    cout << getLCA(root, a, b);
+    delete root;
+}
